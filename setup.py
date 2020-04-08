2781dc835a2beb6bc48a174081cc864d218892c2
Bea Steers <bsteers@nyu.edu>
2020-04-08 17:34:16 -0400

    bump to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='ocycle',
-                 version='0.0.1',
+                 version='0.0.2',
                  description='Stream buffering and event triggering for unbalanced frame sizes.',
                  long_description=open('README.md').read().strip(),
                  long_description_content_type='text/markdown',
